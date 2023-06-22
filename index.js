922c5944d88166b85761e81d4f9af5990d13e06d
Sam Zofkie <samzofkie@gmail.com>
2023-06-22 10:20:49 -0500

    Added < and > handling.

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var iframeOutput = document.getElementById("textoutput");
 
 function Parser(markdown) {
   let outputHtml = "";
-  let codeTagParity = 0, underlineTagParity = 0; 
+  let codeTagParity = 0, 
+      preTagParity = 0, 
+      underlineTagParity = 0; 
   for (let i=0; i<markdown.length; i++) {
     
     if (markdown[i] == '#' && markdown[i+1] == ' ') {
@@ -30,15 +32,29 @@ function Parser(markdown) {
       underlineTagParity = (underlineTagParity + 1) % 2;
     
     } else if (markdown[i] == "`") {
+      if (markdown[i-1] == "\n") {
+        outputHtml += "<pre>";
+        preTagParity = 1;
+      }
       if (codeTagParity == 0)
-        outputHtml += "<pre><code>"
+        outputHtml += "<code>"
       else {
-        outputHtml += "</code></pre>";
+        outputHtml += "</code>";
+        if (preTagParity == 1) {
+          outputHtml += "</pre>";
+          preTagParity = 0;
+        }
       }
       codeTagParity = (codeTagParity + 1) % 2;
    
     } else if (markdown[i] == "\n") {
       outputHtml += "<br>";
+
+    } else if (markdown[i] == "<") {
+      outputHtml += "&lt";
+
+    } else if (markdown[i] == ">") {
+      outputHtml += "&gt";
 
     } else {
       outputHtml += markdown[i];
