b806fa4fc8ebb85cc107168910e1464e3587c2e2
Sam Zofkie <samzofkie@gmail.com>
2023-06-30 19:48:19 -0500

    Added tab support.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { createRoot } from "react-dom/client";
 import Compiler from "./Compiler.js";
 
 // TODO
-// * tab key
 // * html download button
 // * incremental rendering
 // * font / color syntax
@@ -15,15 +14,31 @@ import Compiler from "./Compiler.js";
 const exampleString =
   "# Header\nNormal text\nLine breaks __underlined__ `code doesn't do # header or __underline__`\n`code block if u start on a new line\n  code block respects\n    indentation   and   white   space __# `\n* bullet\n* points";
 
-let compiler = new Compiler();
+function handleTextareaTab(e) {
+  if (e.key == "Tab") {
+    e.preventDefault();
+    var start = e.target.selectionStart;
+    var end = e.target.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    e.target.value =
+      e.target.value.substring(0, start) + "  " + e.target.value.substring(end);
+
+    // put caret at right position again
+    e.target.selectionStart = e.target.selectionEnd = start + 2;
+  }
+}
 
 function MyApp() {
   const [note, setNote] = useState(exampleString);
+
+  let compiler = new Compiler();
 
   return (
     <>
       <textarea
         value={note}
+        onKeyDown={handleTextareaTab}
         onChange={(e) => setNote(e.target.value)}
         spellCheck={false}
         id={"notearea"}
